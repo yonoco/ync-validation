b34688ea2ea46eebfc738ecdf5a60a6e22595cfd
tfitos <tamas.fitos@gmail.com>
2014-12-31 18:50:22 +0100

    example1: body-parser added to receive the data which will be validated

--- examples/example1_webapp/webapp.js
+++ examples/example1_webapp/webapp.js
@@ -1,12 +1,23 @@
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.get('/', function(req, res) {
     res.render('index');
+});
+
+app.post('/signup', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('you posted:\n');
+    res.end(JSON.stringify(req.body, null, 2));
 });
 
 app.listen(4000);
