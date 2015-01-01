ad6c005ae6639e16dc3068e4a7c10f6a8e8378e1
tfitos <tamas.fitos@gmail.com>
2015-01-02 00:31:39 +0100

    example1: email format validator

--- examples/example1_webapp/validator.js
+++ examples/example1_webapp/validator.js
@@ -5,25 +5,24 @@ var FailedResult = validation.FailedResult;
 
 module.exports = function(obj, callback) {
 
-	function RequiredValidator(field, failedResult) {
+	function RequiredValidator(field) {
 		var validateFn = function validate(obj) {
 			return !!obj[field];
 		};
-		Validator.call(this, validateFn, failedResult);
+		Validator.call(this, validateFn, new FailedResult(field, 'ERROR', 'required'));
 	}
 
 	RequiredValidator.prototype = new Validator();
 
-	var emailRequiredValidator = new RequiredValidator(
-		'email',
-		new FailedResult('email', 'ERROR')
-	);
+	var emailRequiredValidator = new RequiredValidator('email');
 
-	var passwordRequiredValidator = new RequiredValidator(
-		'password',
-		new FailedResult('password', 'ERROR')
-	);
+	var passwordRequiredValidator = new RequiredValidator('password');
 
-	validate(obj, [emailRequiredValidator, passwordRequiredValidator], callback);
+	var emailFormatValidator = new Validator(function validate(obj) {
+		var field = obj.email;
+		return !field || (field.indexOf('@') >= 0 && field.indexOf('.') >= 0 && field.lastIndexOf('@') < field.lastIndexOf('.'));
+	}, new FailedResult('email', 'ERROR', 'email'));
+
+	validate(obj, [emailRequiredValidator, emailFormatValidator, passwordRequiredValidator], callback);
 
 }
